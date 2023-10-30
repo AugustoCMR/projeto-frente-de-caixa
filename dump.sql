fedd052da962b69b10f7b4a6537481cd8e01279e
AugustoCMR <augustoribeiro100.g.j@gmail.com>
2023-10-30 20:31:51 -0300

    Atualizando o banco de dados

--- dump.sql
+++ dump.sql
@@ -1,24 +1,61 @@
-CREATE DATABASE pdv
+CREATE DATABASE pdv 
 
-create table usuarios(
-	id serial primary key unique,
-  nome text,
-  email text unique,
-  senha text
-);
+create table
+    usuarios(
+        id serial primary key unique,
+        nome text,
+        email text unique,
+        senha text
+    );
 
-create table categorias(
-	id serial primary key unique,
-  descricao text
-);
+create table
+    categorias(
+        id serial primary key unique,
+        descricao text
+    );
 
-insert into categorias(descricao)values
-('Informática'),
-('Celulares'), 
- ('Beleza e Perfumaria'), 
- ('Mercado'), 
- ('Livros e Papelaria'),
-('Brinquedos'),
-('Moda'),
-('Bebê'),
-('Games')
+insert into
+    categorias(descricao)
+values ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), ('Livros e Papelaria'), ('Brinquedos'), ('Moda'), ('Bebê'), ('Games')
+
+CREATE TABLE
+    produtos(
+        id serial primary key unique,
+        descricao text,
+        quantidade_estoque text,
+        valor numeric,
+        categoria_id integer references categorias(id)
+    );
+
+CREATE TABLE
+    clientes(
+        id serial primary key unique,
+        nome text,
+        email text unique,
+        cpf text unique,
+        cep text,
+        rua text,
+        numero text,
+        bairro text,
+        cidade text,
+        estado text
+    );
+
+create table
+    pedidos(
+        id serial primary key,
+        cliente_id int references clientes(id),
+        observacao text,
+        valor_total int
+    );
+
+create table
+    pedido_produtos(
+        id serial primary key,
+        pedido_id int references pedidos(id),
+        produto_id int references produtos(id),
+        quantidade_produto int,
+        valor_produto int
+    );
+
+ALTER TABLE produtos ADD COLUMN produto_imagem text;
