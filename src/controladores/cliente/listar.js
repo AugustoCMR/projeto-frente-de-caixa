2af4b6bc269fd42fa1a9cf413c87d36942c43215
AugustoCMR <augustoribeiro100.g.j@gmail.com>
2023-10-31 20:05:30 -0300

    Criando rota listar clientes

---
+++ src/controladores/cliente/listar.js
@@ -0,0 +1,18 @@
+const knex = require("../../conexao");
+
+const listarClientes = async (req, res) => {
+    
+    
+
+    try {
+        console.log("oi")
+        const listaClientes = await knex("clientes").select("*");
+
+        return res.status(200).json(listaClientes);
+
+    } catch (error) {
+        return res.status(500).json({mensagem: `Ocorreu um erro interno do servidor: ${error.message}`});
+    }
+}
+
+module.exports = listarClientes;
--- src/rotas/cliente.js
+++ src/rotas/cliente.js
@@ -2,9 +2,11 @@ const express = require("express");
 const rotas = express.Router();
 const cadastrarCliente = require("../controladores/cliente/cadastrar");
 const editarCliente = require("../controladores/cliente/editar");
+const listarClientes = require("../controladores/cliente/listar");
 
 rotas.post("/cliente", cadastrarCliente);
 rotas.put("/cliente/:id", editarCliente);
+rotas.get("/cliente", listarClientes);
 
 
 module.exports = rotas
