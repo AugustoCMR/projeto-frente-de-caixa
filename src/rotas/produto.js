e1732e42a9074eb66d5340e523f9ebbe39ed1f3d
AugustoCMR <augustoribeiro100.g.j@gmail.com>
2023-10-30 21:12:25 -0300

    Criando rota detalhar produto

--- src/rotas/produto.js
+++ src/rotas/produto.js
@@ -3,9 +3,11 @@ const rotas = express.Router();
 const cadastrarProduto = require("../controladores/produto/cadastrar");
 const editarProduto = require("../controladores/produto/editar");
 const listarProdutos = require("../controladores/produto/listar");
+const detalharProduto = require("../controladores/produto/detalhar");
 
 rotas.post("/produto", cadastrarProduto);
 rotas.put("/produto/:id", editarProduto);
 rotas.get("/produto", listarProdutos);
+rotas.get("/produto/:id", detalharProduto);
 
 module.exports = rotas
